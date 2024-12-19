a3573c7bd69b7c6dea0b0f6702cdf4723d4ceb44
Zmallwood <zmallwood@proton.me>
2024-12-19 13:55:00 +0100

    Add project structure files

---
+++ src/server/src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+int main() {
+
+    std::cout << "Dark World Server\n";
+    std::string s;
+    std::cin >> s;
+    std::cout << "Exiting\n";
+    std::cin >> s;
+
+    return 0;
+}
