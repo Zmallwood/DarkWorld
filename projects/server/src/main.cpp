3d6de32a6106245eb692463df5fc3a2334519593
Zmallwood <zmallwood@proton.me>
2024-12-21 08:59:30 +0100

    Save

--- projects/server/src/main.cpp
+++ projects/server/src/main.cpp
@@ -1,14 +1,5 @@
-#include <boost/beast/core.hpp>
-#include <boost/beast/websocket.hpp>
-#include <boost/asio/strand.hpp>
-#include <algorithm>
-#include <cstdlib>
-#include <functional>
-#include <iostream>
-#include <memory>
-#include <string>
-#include <thread>
-#include <vector>
+#include "core/server.hpp"
+#include "pch/pch.hpp"
 
 namespace beast = boost::beast;         // from <boost/beast.hpp>
 namespace http = beast::http;           // from <boost/beast/http.hpp>
@@ -19,105 +10,68 @@ using tcp = boost::asio::ip::tcp;       // from <boost/asio/ip/tcp.hpp>
 //------------------------------------------------------------------------------
 
 // Report a failure
-void
-fail(beast::error_code ec, char const* what)
-{
+void fail(beast::error_code ec, char const *what) {
     std::cerr << what << ": " << ec.message() << "\n";
 }
 
 // Echoes back all received WebSocket messages
-class session : public std::enable_shared_from_this<session>
-{
+class session : public std::enable_shared_from_this<session> {
     websocket::stream<beast::tcp_stream> ws_;
     beast::flat_buffer buffer_;
 
-public:
+  public:
     // Take ownership of the socket
-    explicit
-    session(tcp::socket&& socket)
-        : ws_(std::move(socket))
-    {
+    explicit session(tcp::socket &&socket) : ws_(std::move(socket)) {
     }
 
     // Start the asynchronous operation
-    void
-    run()
-    {
+    void run() {
         // Set suggested timeout settings for the websocket
-        ws_.set_option(
-            websocket::stream_base::timeout::suggested(
-                beast::role_type::server));
+        ws_.set_option(websocket::stream_base::timeout::suggested(beast::role_type::server));
 
         // Set a decorator to change the Server of the handshake
-        ws_.set_option(websocket::stream_base::decorator(
-            [](websocket::response_type& res)
-            {
-                res.set(http::field::server,
-                    std::string(BOOST_BEAST_VERSION_STRING) +
-                        " websocket-server-async");
-            }));
+        ws_.set_option(websocket::stream_base::decorator([](websocket::response_type &res) {
+            res.set(http::field::server, std::string(BOOST_BEAST_VERSION_STRING) + " websocket-server-async");
+        }));
 
         // Accept the websocket handshake
-        ws_.async_accept(
-            beast::bind_front_handler(
-                &session::on_accept,
-                shared_from_this()));
+        ws_.async_accept(beast::bind_front_handler(&session::on_accept, shared_from_this()));
     }
 
-    void
-    on_accept(beast::error_code ec)
-    {
-        if(ec)
+    void on_accept(beast::error_code ec) {
+        if (ec)
             return fail(ec, "accept");
 
         // Read a message
         do_read();
     }
 
-    void
-    do_read()
-    {
+    void do_read() {
         // Read a message into our buffer
-        ws_.async_read(
-            buffer_,
-            beast::bind_front_handler(
-                &session::on_read,
-                shared_from_this()));
+        ws_.async_read(buffer_, beast::bind_front_handler(&session::on_read, shared_from_this()));
     }
 
-    void
-    on_read(
-        beast::error_code ec,
-        std::size_t bytes_transferred)
-    {
+    void on_read(beast::error_code ec, std::size_t bytes_transferred) {
         boost::ignore_unused(bytes_transferred);
 
         // This indicates that the session was closed
-        if(ec == websocket::error::closed)
+        if (ec == websocket::error::closed)
             return;
 
-        if(ec)
+        if (ec)
             fail(ec, "read");
 
-        std::string s(boost::asio::buffer_cast<const char*>(buffer_.data()), buffer_.size());
+        std::string s(boost::asio::buffer_cast<const char *>(buffer_.data()), buffer_.size());
         std::cout << "Messages recieved: " << s << std::endl;
         // Echo the message
         ws_.text(ws_.got_text());
-        ws_.async_write(
-            buffer_.data(),
-            beast::bind_front_handler(
-                &session::on_write,
-                shared_from_this()));
+        ws_.async_write(buffer_.data(), beast::bind_front_handler(&session::on_write, shared_from_this()));
     }
 
-    void
-    on_write(
-        beast::error_code ec,
-        std::size_t bytes_transferred)
-    {
+    void on_write(beast::error_code ec, std::size_t bytes_transferred) {
         boost::ignore_unused(bytes_transferred);
 
-        if(ec)
+        if (ec)
             return fail(ec, "write");
 
         // Clear the buffer
@@ -131,82 +85,59 @@ public:
 //------------------------------------------------------------------------------
 
 // Accepts incoming connections and launches the sessions
-class listener : public std::enable_shared_from_this<listener>
-{
-    net::io_context& ioc_;
+class listener : public std::enable_shared_from_this<listener> {
+    net::io_context &ioc_;
     tcp::acceptor acceptor_;
 
-public:
-    listener(
-        net::io_context& ioc,
-        tcp::endpoint endpoint)
-        : ioc_(ioc)
-        , acceptor_(ioc)
-    {
+  public:
+    listener(net::io_context &ioc, tcp::endpoint endpoint) : ioc_(ioc), acceptor_(ioc) {
         beast::error_code ec;
 
         // Open the acceptor
         acceptor_.open(endpoint.protocol(), ec);
-        if(ec)
-        {
+        if (ec) {
             fail(ec, "open");
             return;
         }
 
         // Allow address reuse
         acceptor_.set_option(net::socket_base::reuse_address(true), ec);
-        if(ec)
-        {
+        if (ec) {
             fail(ec, "set_option");
             return;
         }
 
         // Bind to the server address
         acceptor_.bind(endpoint, ec);
-        if(ec)
-        {
+        if (ec) {
             fail(ec, "bind");
             return;
         }
 
         // Start listening for connections
-        acceptor_.listen(
-            net::socket_base::max_listen_connections, ec);
-        if(ec)
-        {
+        acceptor_.listen(net::socket_base::max_listen_connections, ec);
+        if (ec) {
             fail(ec, "listen");
             return;
         }
     }
 
     // Start accepting incoming connections
-    void
-    run()
-    {
+    void run() {
         do_accept();
     }
 
-private:
-    void
-    do_accept()
-    {
+  private:
+    void do_accept() {
         // The new connection gets its own strand
-        acceptor_.async_accept(
-            net::make_strand(ioc_),
-            beast::bind_front_handler(
-                &listener::on_accept,
-                shared_from_this()));
+        acceptor_.async_accept(net::make_strand(ioc_),
+                               beast::bind_front_handler(&listener::on_accept, shared_from_this()));
     }
 
-    void
-    on_accept(beast::error_code ec, tcp::socket socket)
-    {
-        if(ec)
-        {
+    void on_accept(beast::error_code ec, tcp::socket socket) {
+        if (ec) {
             fail(ec, "accept");
-        }
-        else
-        {
+        } else {
             // Create the session and run it
             std::make_shared<session>(std::move(socket))->run();
         }
@@ -218,15 +149,14 @@ private:
 
 //------------------------------------------------------------------------------
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char *argv[]) {
+    forr::server server;
+    server.run();
     // Check command line arguments.
-    if (argc != 4)
-    {
-        std::cerr <<
-            "Usage: websocket-server-async <address> <port> <threads>\n" <<
-            "Example:\n" <<
-            "    websocket-server-async 0.0.0.0 8080 1\n";
+    if (argc != 4) {
+        std::cerr << "Usage: websocket-server-async <address> <port> <threads>\n"
+                  << "Example:\n"
+                  << "    websocket-server-async 0.0.0.0 8080 1\n";
         return EXIT_FAILURE;
     }
     auto const address = net::ip::make_address(argv[1]);
@@ -242,12 +172,8 @@ int main(int argc, char* argv[])
     // Run the I/O service on the requested number of threads
     std::vector<std::thread> v;
     v.reserve(threads - 1);
-    for(auto i = threads - 1; i > 0; --i)
-        v.emplace_back(
-        [&ioc]
-        {
-            ioc.run();
-        });
+    for (auto i = threads - 1; i > 0; --i)
+        v.emplace_back([&ioc] { ioc.run(); });
     ioc.run();
 
     return EXIT_SUCCESS;
--- projects/server/xmake.lua
+++ projects/server/xmake.lua
@@ -2,6 +2,13 @@ add_requires("nlohmann_json")
 add_requires("boost", {configs = {beast=true}})
 target("DarkWorldServer")
     set_kind("binary")
-    add_files("src/*.cpp")
+    add_files("src/**.cpp")
     add_packages("nlohmann_json")
     add_packages("boost", {components="beast"})
+    set_pcxxheader("src/pch/pch.hpp")
+    add_cxxflags(
+      "-Wall",
+      "-Wextra",
+      "-Werror",
+      { force = true }
+    )
